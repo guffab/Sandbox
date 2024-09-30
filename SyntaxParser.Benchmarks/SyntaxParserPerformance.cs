6f1dd8e64a95e9d8f20e6cc6f4c713d8f33dba94
guffab <guflerfabian02@gmail.com>
2024-09-30 17:46:17 +0200

    -- update for consistency

--- SyntaxParser.Benchmarks/SyntaxParserPerformance.cs
+++ SyntaxParser.Benchmarks/SyntaxParserPerformance.cs
@@ -20,43 +20,37 @@ namespace SyntaxParser.Benchmarks
         [Benchmark]
         public int Unsplittable()
         {
-            int count = 0;
-
-            foreach (var a in syntaxParser.Split(" (\"if(,,)\") "))
-                count++;
-
-            return count;
+            ReadOnlySpan<char> input = " (\"if(,,)\") ";
+            return RunTest(input);
         }
 
         [Benchmark]
         public int UnsplittableV2()
         {
-            int count = 0;
-
-            foreach (var a in syntaxParser.Split(" if(Path(\"@Host.Paramters[Definition.BuiltInParameter(DB_COLOR_RVT)].AsValueString\"), Path(\"@Host.Paramters[Definition.BuiltInParameter(DB_COLOR_RVT)].AsValueString\") + 24, Path(\"@Host.Paramters[Definition.BuiltInParameter(DB_COLOR_RVT)].AsValueString\") - 13"))
-                count++;
-
-            return count;
+            ReadOnlySpan<char> input = " if(Path(\"@Host.Paramters[Definition.BuiltInParameter(DB_COLOR_RVT)].AsValueString\"), Path(\"@Host.Paramters[Definition.BuiltInParameter(DB_COLOR_RVT)].AsValueString\") + 24, Path(\"@Host.Paramters[Definition.BuiltInParameter(DB_COLOR_RVT)].AsValueString\") - 13";
+            return RunTest(input);
         }
 
         [Benchmark]
         public int Splittable()
         {
-            int count = 0;
-
-            foreach (var a in syntaxParser.Split("Path(((()))), 313, 12"))
-                count++;
-
-            return count;
+            ReadOnlySpan<char> input = "Path(((()))), 313, 12";
+            return RunTest(input);
         }
 
         [Benchmark]
         public int SplittableV2()
         {
+            ReadOnlySpan<char> input = "    Path(\"@Host.Paramters[Definition.BuiltInParameter(DB_COLOR_RVT)].AsValueString\"), Path(\"@Host.Paramters[Definition.BuiltInParameter(DB_COLOR_RVT)].AsValueString\") + 24, Path(\"@Host.Paramters[Definition.BuiltInParameter(DB_COLOR_RVT)].AsValueString\") - 13";
+            return RunTest(input);
+        }
+
+        private int RunTest(ReadOnlySpan<char> input)
+        {
             int count = 0;
 
-            foreach (var a in syntaxParser.Split("    Path(\"@Host.Paramters[Definition.BuiltInParameter(DB_COLOR_RVT)].AsValueString\"), Path(\"@Host.Paramters[Definition.BuiltInParameter(DB_COLOR_RVT)].AsValueString\") + 24, Path(\"@Host.Paramters[Definition.BuiltInParameter(DB_COLOR_RVT)].AsValueString\") - 13"))
-                count++;
+            foreach (var a in syntaxParser.Split(input))
+                count += a.Length;
 
             return count;
         }
