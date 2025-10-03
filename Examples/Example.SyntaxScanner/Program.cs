aab77e228b80cebf103d9af8775571119abd4f49
guffab <guflerfabian02@gmail.com>
2025-10-03 16:22:38 +0200

    break build to test yaml files

--- Examples/Example.SyntaxScanner/Program.cs
+++ Examples/Example.SyntaxScanner/Program.cs
@@ -3,11 +3,11 @@
 var example1 = "Disassemble[Parameters[Definition.Name((PF_Number)|(Number))].AsElementId.Name(^Ass)].Name";
 var example2 = "Disassemble[Parameters[Definition.Name((PF_Number)|(Number)).AsElementId.Name(^Ass)].Name";
 var example3 = "Disassemble[Parameters[Definition.Name((PF_Number)|(Number)].AsElementId.Name(^Ass)].Name";
-
+#error dd
 var aa = ObjectPathParser.Parse(example1.AsSpan());
 var bb = ObjectPathParser.Parse(example2.AsSpan());
 var cc = ObjectPathParser.Parse(example3.AsSpan());
-
+zcu hjnimk,rol.edxp
 var av = aa?.IsValid() ?? false; //expected: true
 var bv = bb?.IsValid() ?? false; //expected: false
 var cv = cc?.IsValid() ?? false; //expected: false
