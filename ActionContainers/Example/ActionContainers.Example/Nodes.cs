b027de5b298e9aec6daad61c9bb01901f0daa528
guffab <guflerfabian02@gmail.com>
2025-10-18 11:48:21 +0200

    readonly

--- ActionContainers/Example/ActionContainers.Example/Nodes.cs
+++ ActionContainers/Example/ActionContainers.Example/Nodes.cs
@@ -2,8 +2,8 @@ using Newtonsoft.Json;
 
 namespace ActionContainers.Example;
 
-record TemplateAction([JsonProperty("I")] string Id, [JsonProperty("P")] List<TemplateParameter> Parameters, [JsonProperty("T")] List<TypeAction> Types);
+record TemplateAction([JsonProperty("I")] string Id, [JsonProperty("P")] IReadOnlyList<TemplateParameter> Parameters, [JsonProperty("T")] IReadOnlyList<TypeAction> Types);
 
-record TemplateParameter([JsonProperty("I")] string Id, [JsonProperty("U")] string Unit);
+record TemplateParameter([JsonProperty("I")] string Id, [JsonProperty("U")] string Unit = "");
 
-record TypeAction([JsonProperty("I")] string Id, [JsonProperty("V")] List<string> ParameterValues);
+record TypeAction([JsonProperty("I")] string Id, [JsonProperty("V")] IReadOnlyList<string> ParameterValues);
--- ActionContainers/Example/ActionContainers.Example/Program.cs
+++ ActionContainers/Example/ActionContainers.Example/Program.cs
@@ -1,50 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
 using ActionContainers;
+using ActionContainers.Example;
+using Newtonsoft.Json;
 
 Console.WriteLine("Hello, World!");
 
+List<TemplateAction> a = [new TemplateAction("Production Unit", [new("Layer 1"), new("Layer 2"), new("Flip Layer 1", "bool")],
+[new("Filigree Slab", ["Layer_@_Filigree", "", "0"]), new("Double Wall", ["", "Layer_@_Wall 2", "0"])])];
+var serialized = JsonConvert.SerializeObject(a, Formatting.Indented);
+Console.WriteLine(serialized);
 
-var example1 = /*lang=jsonc,strict*/ """
-    [
-        {
-            "I": "Production Unit",
-            "P": [
-                {
-                    "I": "Layer 1"
-                },
-                {
-                    "I": "Layer 2"
-                },
-                {
-                    "I": "Flip Layer 1",
-                    "U": "bool"
-                }
-            ],
-            "T": [
-                {
-                    "I": "Filigree Slab",
-                    "P": [
-                        "Layer_@_Filigree",
-                        "",
-                        "0"
-                    ]
-                },
-                {
-                    "I": "Double Wall",
-                    //Represents the value of each parameter connected by its index => saves a lot of space (no key duplication), although each parameter needs a value assigned even if it's default!
-                    //Rename: O(1) => template/types not involved
-                    //Create: O(1) * t*O(1) = O(1) => instant by appending, all types have to append as well
-                    //Read/Update: O(1)
-                    //Delete: O(n)
-                    "P": [
-                        "",
-                        "Layer_@_Wall 2",
-                        "0"
-                    ]
-                }
-            ]
-        }
+var deserialized = JsonConvert.DeserializeObject<List<TemplateAction>>(serialized);
+
+var example1 = /*lang=json*/ """
+[
+  {
+    "Id": "Production Unit",
+    "Parameters": [
+      {
+        "Id": "Layer 1",
+      },
+      {
+        "Id": "Layer 2",
+      },
+      {
+        "Id": "Flip Layer 1",
+        "Unit": "bool"
+      }
+    ],
+    "Types": [
+      {
+        "Id": "Filigree Slab",
+        "ParameterValues": [
+          "Layer_@_Filigree",
+          "",
+          "0"
+        ]
+      },
+      {
+        "Id": "Double Wall",
+        
+        //Represents the value of each parameter connected by its index => saves a lot of space (no key duplication), although each parameter needs a value assigned even if it's default!
+        //Rename: O(1) => no types involved
+        //Create: (t + 1)*O(1) = O(1) => instant by appending, all types have to append as well
+        //Read/Update: O(1)
+        //Delete: O(n)
+        "ParameterValues": [
+          "",
+          "Layer_@_Wall 2",
+          "0"
+        ]
+      }
     ]
-    """;
-
-Console.WriteLine(example1);
+  }
+]
+""";
