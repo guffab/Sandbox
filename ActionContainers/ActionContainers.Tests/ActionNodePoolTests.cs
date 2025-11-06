284b2f6ebb586f357070f2c879af7b11ecddf27f
guffab <guflerfabian02@gmail.com>
2025-11-06 10:56:15 +0100

    -

--- ActionContainers/ActionContainers.Tests/ActionNodePoolTests.cs
+++ ActionContainers/ActionContainers.Tests/ActionNodePoolTests.cs
@@ -52,7 +52,7 @@ public class ActionNodePoolTests
             ]
         }
         """;
- 
+
         //Assert
         Assert.Throws<InvalidDataException>(() => JsonConvert.DeserializeObject<ActionNode>(serialized));
     }
